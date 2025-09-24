9e62a73063fd82241b0fac61d7d37a72305daf5d
glorymary96 <glorym.96@gmail.com>
2025-09-24 14:56:20 +0200

    Initial commit

---
+++ data/fetch_data.py
@@ -0,0 +1,61 @@
+import yfinance as yf
+import pandas as pd
+
+def download_stock_prices(tickers: list,
+                  start_date:str,
+                   end_date:str,
+                  auto_adjust:bool = True,
+                  price_field:str = 'Close'):
+
+    # To handle single tickers not given as list
+    if isinstance(tickers, str):
+        tickers = [tickers]
+
+    data = yf.download(
+        tickers=tickers,
+        start=start_date,
+        end=end_date,
+        auto_adjust=auto_adjust,
+        progress=False)
+
+    if isinstance(data.columns, pd.MultiIndex):
+        if price_field not in data.columns.levels[0]:
+            raise ValueError(f"Price field '{price_field}' not found in data.")
+        prices = data[price_field].copy()
+    else:
+        if price_field not in data.columns:
+            raise ValueError(f"Price field '{price_field}' not found in data.")
+        prices = data[price_field].copy()
+        prices.columns = [tickers]
+
+    # Handling missing tickers
+    exists = [tic for tic in tickers if tic in prices.columns]
+    prices = prices.reindex(columns = exists).sort_index()
+    prices = prices.dropna().ffill()
+
+    return prices
+
+def load_stock_prices(tickers,start_date:str,end_date:str,auto_adjust:bool = True):
+    data = yf.download(
+        tickers,
+        start=start_date,
+        end=end_date,
+        auto_adjust=auto_adjust,
+        progress=False,
+    )["Close"]
+
+    data = data.dropna(how="all").sort_index().ffill().reset_index()
+    data = data.rename_axis(None, axis=1)
+
+    return data
+
+
+if __name__ == '__main__':
+    tickers = ["META", "AMZN", "AAPL", "NFLX", "GOOGL"]
+    tickers = "META"
+    start_date = "2024-01-01"
+    end_date = "2024-12-31"
+
+    data = load_stock_prices(tickers, start_date, end_date)
+
+    print(data.head())
